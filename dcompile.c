61849bc59f540b8d370b573767b0f3557a489c10
Andrew Smith <drew@Andrews-MacBook-Air.local>
2023-02-26 12:40:17 -0700

    Initial commit

---
+++ dcompile.c
@@ -0,0 +1,71 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
+
+// The format of each instruction is below:
+// <command> <argument1> <argument2> <argument3> ...
+
+void compile(FILE * inFile, FILE * outFile);
+
+void printHelp() {
+	printf("Usage: ./dcompile -i <input file> -o <output file>\n");
+}
+
+int main(int argc, char ** argv) {
+	int opt;
+	char * inputFile = 0;
+	char * outputFile = 0;
+      
+    // put ':' in the starting of the
+    // string so that program can 
+    // distinguish between '?' and ':' 
+    while((opt = getopt(argc, argv, "i:o:")) != -1) { 
+		switch(opt) { 
+		case 'i':
+			inputFile = optarg;
+			break;
+		case 'o':
+			outputFile = optarg;
+			break;
+		case ':': 
+			printf("Option needs a value\n"); 
+			break; 
+		case '?': 
+			printf("Unknown option: %c\n", optopt);
+			break; 
+		} 
+	}
+
+	if(inputFile == 0 || outputFile == 0) {
+		printf("Missing options.\n");
+		printHelp();
+		return 1;
+	}
+
+	FILE * inFile = fopen(inputFile, "r");
+	FILE * outFile = fopen(outputFile, "wb");
+
+	if(!inFile) {
+		printf("Could not find input file: %s\n", inputFile);
+	}
+
+	if(!outFile) {
+		printf("Could not find output file: %s\n", outputFile);
+	}
+
+	compile(inFile, outFile);
+}
+
+
+
+
+void compile(FILE * inFile, FILE * outFile) {
+	char * line = (char *) malloc(500 * sizeof(char));
+	size_t len = 500;
+	uint32_t read;
+
+	while ((read = getline(&line, &len, inFile)) != -1) {
+
+	}
+}
---
+++ makefile
@@ -0,0 +1,24 @@
+Exec = main
+OPTS = -O3 -pthread -g
+
+objects := $(patsubst %.c,%.o,$(wildcard *.c))
+
+GXX = gcc
+
+.PHONY: all
+all: $(Exec)
+
+.PHONY: check
+check:
+	echo Objects are $(objects)
+
+$(objects): %.o: %.c *.h
+	$(GXX) -c $(OPTS) $< -o $@
+
+$(Exec): $(objects)
+	$(GXX) $(OPTS) $(objects) -o $(Exec)
+	echo $(GXX) $(OPTS) $(objects) -o $(Exec)
+
+.PHONY: clean
+clean:
+	-rm *.o $(Exec)
